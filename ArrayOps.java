c6ea30f1de5f9cee17a0641ed4e6e7e68292c522
Mai Koch <mai.koch@post.runi.ac.il>
2024-03-24 23:36:23 +0200

    test

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,120 +1,97 @@
-
-import java.util.*;
-
 public class ArrayOps {
     public static void main(String[] args) {
-        /* testing 
-        int result = findMissingInt(array);
-        System.out.println(result);
-        int secondMaxValue = secondMaxValue(array);
-        System.out.println(secondMaxValue);
-        boolean same = containsTheSameElements(array1, array2);
-        System.out.println(same);
-        boolean sort = isSorted(array1);
-        System.out.println(sort);
-         */
+        //int [] str1 = {1,2,2,2,2};
+        //int [] str2 = {2,1};
+        System.out.println(secondMaxValue(new int [] {2,8,3,7,8})); 
+        //System.out.println(containsTheSameElements(str1,str2));
+        //System.out.println(findMissingInt(str1));
+        // System.out.println(containsTheSameElements(new int [] {3,-4,1,2,5}, new int [] {1,3,-4,5}));
+        // System.out.println(isSorted(new int [] {1,3,2}));
+    }
+    
+    public static int findMissingInt (int [] array) {
+      for (int i = 0; i <= array.length; i++){
+        if (!contains(array, i)){
+            return i;
+        }
+      }
+      return 0;
     }
 
-    public static int findMissingInt (int [] array) {
-        int[] order;
-        order = new int [array.length+1];
-        Arrays.fill(order, -1);
-        for (int i=0; i < array.length;i++)
-        {
-            int val = array[i];
-            order[val] = 0;
+    public static boolean contains (int [] array, int value){
+        for (int i = 0; i < array.length; i++){
+            if (value == array[i]){
+                return true;
+            }
         }
-        int index = 0;
-        while(order[index] == 0) {
-            index = index + 1 ;
-        } return index;
+        return false;
     }
+    
 
     public static int secondMaxValue(int [] array) {
-        int max1 = array[0];
-        int max2 = array[1];
-        if (max1 < max2) {
-            int temp = max1;
-            max1 = max2;
-            max2 = temp;
+        int max = array [0];
+        for (int i = 1; i < array.length; i++){ //finding the max value 
+            if (max < array[i]){
+                max = array[i];
+            }  
         }
-        if(array.length > 2) {
-            for (int i=2; i < array.length; i++) {
-                if(array[i] > max2 && array[i] < max1) {
-                    max2 = array[i];
-                }
+        if (countTimes(array, max)>1){
+            return max;
+        }
+        int secondMax = array [0]; //checking if the first element is the max 
+        if ( secondMax != max){
+            secondMax = array [0]; 
+        }
+        else secondMax = array [1];
+        for (int j = 1; j < array.length; j++){ // finding the second max 
+            if (secondMax<array[j] && array[j]<max){
+                secondMax= array[j];
             }
-        } return max2;
+        }
+        return secondMax;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        boolean [] arr1 = new boolean[array1.length];
-        boolean [] arr2 = new boolean[array2.length];
-        boolean same = true;
-        int index1 = 0;
-        int index2 = 0;
-        for (int p=0; p < array2.length; p++) {
-            arr2[p] = false;
+    public static int countTimes(int [] array, int value){
+        int count = 0; 
+        for (int i = 0; i <array.length; i++){
+            if (array[i]==value){
+                count++;
+            }
         }
-        for (int i=0; i < array1.length; i++) {
-            int countT = 0;
-            for (int j=0; j < array2.length; j++) {
-                if(array1[i]==array2[j]) {
-                    if (countT == 0) {
-                        arr1[i] = true;
-                        arr2[j] = true;
-                        countT = 1;
-                    } else {
-                        arr2[j] = true;
-                    }
-                }else {
-                    if((countT == 0) && (arr2[j] = true)) {
-                        arr1[i] = false;
-                    }
-                }
-            }
-        } for (int i=0; i < arr1.length; i++) {
-            if(arr1[i]) {
-                index1 = index1 + 1;
-            }
-        } if(index1 == arr1.length) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr2[j]) {
-                    index2++;
-                }
-            }
-            same = index2 == arr2.length;
-        } else {
-            same = false;
-        } return same;
+        return count;
     }
+    public static boolean containsTheSameElements(int [] array1 , int [] array2) {
+        for (int i = 0; i < array1.length; i++){
+            //compers the first array to the second
+            if (!contains(array2, array1[i])){
+                return false;
+        }
+    }
+        for (int j = 0; j < array2.length; j++){
+            //compers the second array to the first
+            if (!contains(array1, array2[j])){
+                return false;
+        }
+    }
+        return true;
+}
+
     public static boolean isSorted(int [] array) {
-        boolean sort = true;
-        int x = array[0];
-        int count = 0;
-        for (int i=1; i < array.length; i++) {
-            if (x > array[i]) {
-                count = count + 1;
-                x = array[i];
-            } else {
-                if (count == 0) {
-                    x = array[i];
-                    count = count + 1;
-                    for (int j = 2; j < array.length; j++) {
-                        if (array[j] > x) {
-                            x = array[j];
-                            count = count + 1;
-                        } else {
-                            sort = false;
-                            j = array.length;
-                        }
-                    }
-                } else {
-                    sort = false;
-                } i = array.length;
+        if (array[0]>array[array.length-1]){
+            for (int i = 0; i < array.length-1; i++){
+                if (array[i]<array[i+1]){
+                    return false;
+                }
             }
-        } if(count == array.length - 1) {
-            sort = true;
-        } return sort;
+
+        }
+        if (array[0]<array[array.length-1]){
+            for (int i = 0; i < array.length-1; i++){
+                if (array[i]>array[i+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- StringOps.java
+++ StringOps.java
@@ -22,140 +22,109 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        /* testing
-        String testforcapvowel = "heLlo World" ;
-        System.out.println(capVowelsLowRest(testforcapvowel));
-        String testforcamelCase = "   tHis is test";
-        System.out.println(camelCase(testforcamelCase));
-        String allindex = "  this will be a test";
-        char chr = 't';
-        String result = "The letter " + chr +" shows in the string " + "(" + allindex + ")" + " in index:";
-        int[] array = allIndexOf(allindex,chr);
-        for (int i=0; i < array.length; i++) {
-            result = result + " " + i + " ";
-        }
-        System.out.println(result);
-         */
-    }
-    
-    public static String capVowelsLowRest (String string) {
-        String output = "";
-        int length = string.length();
-        for (int i = 0; i < length ; i++) {
-            char letter = string.charAt(i);
-            int ch = letter;
-            //checks if the character is uppercase
-            if ((65 <= ch) && (ch <= 90)) {
-                //is it an uppercase vowel?
-                if (ch == 65 || ch == 69 || ch == 73 || ch == 79 || ch == 85) {
-                    //keeps uppercase vowel
-                    output = output + letter;
-                }
-                else {
-                    //convert to lowercase consonant
-                    output = output + (char) (ch + 32);
-                }
-            }
-            else { //character is lowercase or space
-                if (((97 <= ch) && (ch <= 122)) || (ch == 32)) {
-                    //is it a lowercase vowel?
-                    if (ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117) {
-                        //convert lower vowel to uppercase
-                        output = output + (char) (ch - 32);
-                    }
-                    else {
-                        //keeps lowercase consonant or the space
-                        output = output + letter;
-                    }
-                }
-            }
-        } return output;
+        // String string = args [0]; 
+        // String ch = args [1];
+        // char chr = ch.charAt(0);
+        // System.out.println(capVowelsLowRest(string));
+        //String string = "Hello World";
+        //char chr = 'l';
+        //printArray(allIndexOf(string,chr));
+        System.out.println(camelCase(" tWo    word"));
     }
 
+    public static String capVowelsLowRest (String string) {
+        String newString = "";
+        for (int i = 0; i < string.length(); i++){
+            if ("a,e,i,o,u,A,E,I,O,U".indexOf(string.charAt(i)) != -1){//its a vowel
+                if (string.charAt(i)>='a' && string.charAt(i)<='z'){
+                    newString= newString + (char)(string.charAt(i)- 32); 
+                }
+                else newString= newString + (char) (string.charAt(i));
+            }
+            else {//its not a vowel
+                if (string.charAt(i)>='A' && string.charAt(i)<='Z'){
+                    newString= newString + (char) (string.charAt(i)+32); 
+                }
+                else newString= newString + (char) string.charAt(i);
+            }
+        }
+        return newString;
+    }
 
     public static String camelCase (String string) {
-        String output1 = "";
-        int length = string.length();
-        int index = 0;
-        int spaceCount = 0;
-        boolean space = false;
-        // delete the first sequence of spaces, add to the output the first letter of the first word
-        // and save the index of the first alphabet char in the string
-        for (int i = 0; i < length ; i++) {
-            char letter = string.charAt(i);
-            int ch = letter;
-            if (ch == 32) {
-                space = true;
-            }
-            while (!space)
-            {
-                //if upper, add it to output
-                if ((65 <= ch) && (ch <= 90)) {
-                    output1 = output1 + letter;
-                }
-                else {
-                    //is lower, change to upper and add it to output
-                    output1 = output1 + (char) (ch + 32);
-                }
-                index = i + 1; //save index of 1st char after the first alphabet
-                i = length; //break For loop
-            }
-        }
-        //start checking for spaces, upper and lower from the character after the first alphabet
-        // change/keep all char as lower case unless the char is space, then chance/keep uppercase and ignore the space
-        for (int i = index ; i < length ; i++) {
-            char letter = string.charAt(i);
-            int ch = letter;
-            // checking for spaces
-            if (ch == 32) {
-                spaceCount = spaceCount + 1;
-            }
-            else {
-                if ((65 <= ch) && (ch <= 90))
-                {
-                    //is it an uppercase char?
-                    //convert to lowercase character
-                    if (spaceCount != 0) {
-                        //starting a new word that is not the first word
-                        output1 = output1 + letter;
-                        spaceCount = 0;
-                    }
-                    else output1 = output1 + (char) (ch + 32);
-                }
-                else {
-                    //is lowercase char
-                    if ((spaceCount == 0)) {
-                        //keep lower
-                        output1 = output1 + letter;
-                    }
-                    else {
-                        //convert to uppercase character
-                        output1 = output1 + (char) (ch - 32);
-                        spaceCount = 0;
-                    }
-                }
-            }
-        } return output1;
-    }
+        String lowercaseString = "";
+        String OriginalWOSpaces = deleteFirstSpace(string);//deleting the firt spaces  
+        String newString = "";
 
+        // String subsequence= string.substring(string.charAt(32),string.charAt(32)); 
+        for (int i =0; i < OriginalWOSpaces.length(); i++){
+                if ((OriginalWOSpaces.charAt(i) >= 'A' && OriginalWOSpaces.charAt(i) <= 'Z')){
+                    lowercaseString = lowercaseString +(char)(OriginalWOSpaces.charAt(i)+ 32); 
+                }
+                else{
+                    lowercaseString  = lowercaseString + (char) OriginalWOSpaces.charAt(i);
+                }
+            }
+
+        for (int j = 0; j < lowercaseString.length(); j++){
+               
+                if (lowercaseString.charAt(j) == ' '){
+                    if (lowercaseString.charAt(j+1)!= ' '){
+                        newString = newString + (char) (lowercaseString.charAt(j+1)-32);
+                        j++;}  
+                }
+                else
+                newString = newString + (char) lowercaseString.charAt(j);
+
+            }
+            return newString;
+        }
 
     public static int[] allIndexOf (String string, char chr) {
-        int count = 0;
-        char letter;
-        for (int i = 0; i < string.length(); i++) {
-            letter = string.charAt(i);
-            if (letter == chr){
-                count = count + 1;
+        int [] array = new int [countChar(string, chr)];
+        int index = 0;
+        for (int i = 0; i < string.length(); i++){
+            if (string.charAt(i)== chr){
+                array [index] = i; 
+                index++;
             }
         }
-        int[] arrStr = new int[count];
-        int index = 0;
-        for (int i = 0; i < string.length(); i++) {
-            letter = string.charAt(i);
-            if(letter == chr) {
-                arrStr[index] = i;
-                index = index + 1;
+
+
+        return array;
+    }
+
+    public static int countChar (String string, char chr){
+        int count = 0;
+        for (int i= 0; i < string.length(); i++){
+            if (string.charAt(i)== chr){
+                count++; 
             }
-        }return arrStr;
+        } 
+        return count;
+    }
+
+    public static void printArray(int [] array) {
+        System.out.print("{");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+            char c = i != (array.length -1) ? ',' : '}';
+            System.out.print(c);
+        }
+        System.out.println();
+        
+    }
+    public static String deleteFirstSpace (String string){
+        String newString = "";
+        if (string.charAt(0) == ' '){
+            for (int i = 1; i < string.length();i++){
+                newString += (char) string.charAt(i);
+            }
+            return newString;
+        }
+        else {
+            newString = string;
+        }
+        return newString;
     }
 }
